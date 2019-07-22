8f5fd99127e642a59bbe72165cf7793de20839f1
Jarrod <jarrod.miller@ttu.edu>
2019-07-21 20:18:16 -0500

    Added Dockerrun AWS file, updated db config file.

--- database/config.js
+++ database/config.js
@@ -2,7 +2,9 @@ const { Client } = require('pg');
 const client = new Client({
   user: process.env.PGUSER,
   database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD
+  password: process.env.PGPASSWORD,
+  host: process.env.PGHOST,
+  port: process.env.PGPORT
 });
 
 client.connect()
